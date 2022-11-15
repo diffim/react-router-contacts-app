209ac8991c4021acee7c40868f0f2fb15f22946d
JeweIxx <vhiunese@gmail.com>
2022-11-15 16:28:37 +0400

    almost done with the tutorial, this stuff exciting fr

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+import ErrorPage from "./error-page";
+import Index from "./routes/index";
+import EditContact, { action as editAction } from "./routes/edit";
+
+import "./index.css";
+
+import Contact, { loader as contactLoader } from "./routes/contact";
+
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+
+import DestroyContact, { action as destroyAction } from "./routes/destroy";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        element: <DestroyContact />,
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+      { index: true, element: <Index /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);

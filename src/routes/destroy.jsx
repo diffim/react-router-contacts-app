cdfb4f926698b1190cf6cfcbc4515dcf25cff750
JeweIxx <vhiunese@gmail.com>
2022-11-16 11:01:05 +0400

    finished tutorial

---
+++ src/routes/destroy.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { redirect } from "react-router-dom";
+import { deleteContact } from "../contacts";
+
+export async function action({ params }) {
+  await deleteContact(params.contactId);
+  return redirect(`/`);
+}
+
+function destroy() {
+  return <div>destroy</div>;
+}
+
+export default destroy;
